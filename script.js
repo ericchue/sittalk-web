5edaee4881781ca3fbe60a4e22b1780b19882822
ericchue <ericcwoh@naver.com>
2025-05-21 19:26:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ function saveStory() {
     return;
   }
 
-  // 간단히 로컬스토리지에 저장 (임시)
   localStorage.setItem('sitalk_latest', content);
   alert("이야기가 저장되었습니다!");
 
-  // 메인 페이지로 돌아가기
-  window.location.href = "index.html";
+  location.reload();
 }
